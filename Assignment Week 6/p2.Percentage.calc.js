086ad9be5491b50780b26a063b69223c73b683b6
Hafiz Ahmad Tahseen <150579966+hafizahmadtahseen@users.noreply.github.com>
2023-12-16 18:13:00 +0500

    Add files via upload

---
+++ Assignment Week 6/p2.Percentage.calc.js
@@ -0,0 +1,6 @@
+"use strict";
+// Write a program that calculates the percentage.
+let inp = 50;
+let outp = 200;
+let per = (inp / outp) * 100;
+console.log(`Percentage of ${inp} to ${outp} = ${per} %`);
